5c35cb543c3cd790872831105f529bf4bd2624d0
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-18 20:18:00 +0000

    day2 homework

---
+++ list_dictionary_hw_advanced_logic.py
@@ -0,0 +1,41 @@
+# For the following list of numbers:
+
+numbers = [11, 6, 4, 99, 7, 11]
+
+# 1. Print out a list of the even integers:
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+
+# 2. Print the difference between the largest and smallest value:
+print(max(numbers) - min(numbers))
+
+# 3. Print True if the list contains a 2 next to a 2 somewhere.
+result = False
+index = 0
+for number in numbers:
+    if (number == 2 and numbers[index-1] == 2):
+        result = True
+    index += 1
+print(result)
+
+# 4. Print the sum of the numbers,
+#    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
+#
+#    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+counter = 0
+for number in numbers:
+    if number > 13 or number < 6:
+        counter += number
+
+
+print(counter)
+
+
+# 5. HARD! Print the sum of the numbers.
+#    Except the number 13 is very unlucky, so it does not count.
+#    And numbers that come immediately after a 13 also do not count.
+#    HINT - You will need to track the index throughout the loop.
+#
+#    So [5, 13, 2] would have sum of 5.
